6ca201c51a52a09467d1ea1ef4f8541ffe39aeed
q38007 <460388824@qq.com>
2019-01-11 14:43:56 +0800

    Update App.java

--- src/main/java/com/ghz/testweb/App.java
+++ src/main/java/com/ghz/testweb/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! i am oldboy22333" );
+        System.out.println( "Hello World! i am oldboy1" );
     }
 }
